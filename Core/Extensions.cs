ace22c3a51acfded5b0d0a1f9231fd77a6ff20f3
galen <rarefushion@gmail.com>
2026-05-19 04:00:43 -0700

    Feat: Vector3 FloorDiv and FloorTo Extensions
    Added FloorDiv that extends both Vector3 and Vector3D<int> to divide
    them and account for negative coordinates.
    Added FloorTo that extends both Vector3 and Vector3D<int> that floors
    them to the nearest multiple provided.

--- Core/Extensions.cs
+++ Core/Extensions.cs
@@ -9,4 +9,26 @@ public static class Extensions
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3D<int> Floor(this Vector3 pos) => new((int)MathF.Floor(pos.X), (int)MathF.Floor(pos.Y), (int)MathF.Floor(pos.Z));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3D<int> FloorDiv(this Vector3 pos, int divisor) => new
+    (
+        (int)MathF.Floor(pos.X / divisor),
+        (int)MathF.Floor(pos.Y / divisor),
+        (int)MathF.Floor(pos.Z / divisor)
+    );
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3D<int> FloorDiv(this Vector3D<int> pos, int divisor) => new
+    (
+        MathUtils.FloorDiv(pos.X, divisor),
+        MathUtils.FloorDiv(pos.Y, divisor),
+        MathUtils.FloorDiv(pos.Z, divisor)
+    );
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3D<int> FloorTo(this Vector3 pos, int multiple) => pos.FloorDiv(multiple) * multiple;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3D<int> FloorTo(this Vector3D<int> pos, int multiple) => pos.FloorDiv(multiple) * multiple;
 }
