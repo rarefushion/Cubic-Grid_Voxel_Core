7732989307cdd34f065a2db6e45efb7b7442741d
galen <rarefushion@gmail.com>
2026-05-19 03:49:56 -0700

    Refactor: ChunDims Static Abstract Interfacing
    ChunkDims is now an interface with static abstract types, this allows
    for JIT to know the values ahead of time.
    Created a few default chunk dim types, ChunkDims being the base 16
    length with double, half sizes ect.

--- Core/ChunkDims.cs
+++ Core/ChunkDims.cs
@@ -2,10 +2,40 @@ using System.Numerics;
 
 namespace GalensUnified.CubicGrid.Core;
 
-public readonly struct ChunkDims(int length)
+public interface IChunkDims
 {
     /// <summary>The length of the chunk on one and every axis.</summary>
-    public readonly int Length = length;
+    static abstract int Length { get; }
     /// <summary>The total size of the chunk.</summary>
-    public readonly int Volume = length * length * length;
+    static abstract int Volume { get; }
+}
+
+public readonly struct HalfChunkDims : IChunkDims
+{
+    public static int Length => 8;
+    public static int Volume => 8 * 8 * 8;
+}
+
+public struct ChunkDims : IChunkDims
+{
+    public static int Length => 16;
+    public static int Volume => 16 * 16 * 16;
+}
+
+public readonly struct DoubleChunkDims : IChunkDims
+{
+    public static int Length => 32;
+    public static int Volume => 32 * 32 * 32;
+}
+
+public readonly struct QuadChunkDims : IChunkDims
+{
+    public static int Length => 64;
+    public static int Volume => 64 * 64 * 64;
+}
+
+public readonly struct OctupleChunkDims : IChunkDims
+{
+    public static int Length => 128;
+    public static int Volume => 128 * 128 * 128;
 }
