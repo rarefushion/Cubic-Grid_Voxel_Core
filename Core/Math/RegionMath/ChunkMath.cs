928aed8b81db968be5a8b70dbe6f8930a7828c13
galen <rarefushion@gmail.com>
2026-07-09 00:54:04 -0700

    Performance: ChunkMath BitOperations
    Core/Math/RegionMath/ChunkMath.cs provides mask and shift for bit
    operations.
    It's function math has been replaced to use them.
    ChunkMath requires TDims Length to be a power of 2 or will throw an
    error.

--- Core/Math/RegionMath/ChunkMath.cs
+++ Core/Math/RegionMath/ChunkMath.cs
@@ -1,27 +1,39 @@
+using System.Numerics;
 using Silk.NET.Maths;
 
 namespace GalensUnified.CubicGrid.Core.Math;
 
 public static class ChunkMath<TDims> where TDims : IChunkDims
 {
+    public static readonly int mask;
+    public static readonly int shift;
+
     public static int IndexByGlobalPos(Vector3D<int> pos) =>
         IndexByLocalPos(LocalPosByGlobalPos(pos));
 
     public static int IndexByLocalPos(Vector3D<int> pos) =>
-        (pos.Z * TDims.Length + pos.Y) * TDims.Length + pos.X;
+        (pos.Z << shift | pos.Y) << shift | pos.X;
 
     public static Vector3D<int> LocalPosByGlobalPos(Vector3D<int> pos) => new
-        (
-            ((pos.X % TDims.Length) + TDims.Length) % TDims.Length,
-            ((pos.Y % TDims.Length) + TDims.Length) % TDims.Length,
-            ((pos.Z % TDims.Length) + TDims.Length) % TDims.Length
-        );
+    (
+        pos.X & mask,
+        pos.Y & mask,
+        pos.Z & mask
+    );
 
     public static Vector3D<int> LocalPosByIndex(int index) =>
-        new(index % TDims.Length, index / TDims.Length % TDims.Length, index / (TDims.Length * TDims.Length) % TDims.Length);
+        new(index & mask, (index >> shift) & mask, (index >> (shift * 2)) & mask);
 
     public static bool PosLocal(Vector3D<int> pos) =>
-        pos.X >= 0 && pos.X < TDims.Length &&
-        pos.Y >= 0 && pos.Y < TDims.Length &&
-        pos.Z >= 0 && pos.Z < TDims.Length;
+        (pos.X & ~mask) == 0 &&
+        (pos.Y & ~mask) == 0 &&
+        (pos.Z & ~mask) == 0;
+
+    static ChunkMath()
+    {
+        if (!BitOperations.IsPow2(TDims.Length))
+            throw new Exception($"The IChunkDims.Length provided must be a power of 2. Currently: {TDims.Length}");
+        mask = TDims.Length - 1;
+        shift = BitOperations.TrailingZeroCount(TDims.Length);
+    }
 }
